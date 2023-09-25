8625b0f9107194899a2c68caecde0f19052b36e4
Arno V <versini@gmail.com>
2023-09-24 20:14:29 -0400

    refactor: removing unused components

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,7 +1,6 @@
 import { Button } from "./Button/Button";
-import { ButtonIcon } from "./Button/ButtonIcon";
 import { ButtonLink } from "./Button/ButtonLink";
 import { Footer } from "./Footer/Footer";
 import { Main } from "./Main/Main";
 
-export { Button, ButtonIcon, ButtonLink, Footer, Main };
+export { Button, ButtonLink, Footer, Main };
