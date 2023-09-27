b7c1a7bb06ebea03cdb434c9b67150316e0ee8c9
Arno V <versini@gmail.com>
2023-09-27 10:58:02 -0400

    minor refactor

--- packages/client/src/components/TextInput/TextInput.tsx
+++ packages/client/src/components/TextInput/TextInput.tsx
@@ -1,14 +1,16 @@
-import type { TextInputProps } from "./InputTypes";
+import type { TextInputProps } from "./TextInputTypes";
 
 export const TextInput = ({
 	type,
 	placeholder,
 	onChange,
 	errorMessage,
+	...extraProps
 }: TextInputProps) => {
 	return (
 		<>
 			<input
+				{...extraProps}
 				className="block w-full resize-none rounded-md border-none py-3 px-4 text-base caret-slate-100 focus:outline-none focus:ring-offset-0 focus:ring-2 focus:ring-slate-300 bg-slate-900 text-slate-200 placeholder-slate-400 sm:text-base"
 				type={type}
 				placeholder={placeholder}
