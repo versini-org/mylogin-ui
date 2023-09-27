263a9cd7e167225ac411b1ae39b4050b4939d9b9
Arno V <versini@gmail.com>
2023-09-27 10:55:11 -0400

    Update InputTypes.d.ts

--- packages/client/src/components/TextInput/InputTypes.d.ts
+++ packages/client/src/components/TextInput/InputTypes.d.ts
@@ -1,6 +1,3 @@
 export type TextInputProps = {
 	errorMessage?: string;
-	onChange?: React.ChangeEventHandler<HTMLInputElement>;
-	className?: string;
-	type?: "text" | "password" | "email" | "number" | "search" | "tel" | "url";
 } & React.InputHTMLAttributes<HTMLInputElement>;
