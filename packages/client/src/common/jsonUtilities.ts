8d1029287f02744c6d84a4673f2c48e22e9e78d1
Arno V <versini@gmail.com>
2023-09-29 10:37:23 -0400

    Update jsonUtilities.ts

--- packages/client/src/common/jsonUtilities.ts
+++ packages/client/src/common/jsonUtilities.ts
@@ -1,9 +1,5 @@
 import JSON5 from "json5";
 
 export const jsonParse = (json: string) => {
-	try {
-		return JSON5.parse(json);
-	} catch (e) {
-		return undefined;
-	}
+	return JSON5.parse(json);
 };
