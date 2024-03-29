980f49b3952af74934f1f1c0fca98bfa16ecb3c2
Arno V <versini@gmail.com>
2024-03-29 11:34:37 -0400

    update

--- packages/client/src/modules/App/App.tsx
+++ packages/client/src/modules/App/App.tsx
@@ -1,5 +1,15 @@
-import { ButtonIcon, Footer, Header, Main } from "@versini/ui-components";
-import { IconEdit } from "@versini/ui-icons";
+import {
+	ButtonIcon,
+	Footer,
+	Header,
+	Main,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+} from "@versini/ui-components";
+import { IconDown, IconEdit, IconUp } from "@versini/ui-icons";
 import { useEffect, useReducer, useState } from "react";
 
 import {
@@ -20,6 +30,7 @@ import { reducer } from "./reducer";
 function App() {
 	const storage = useLocalStorage();
 	const [errorMessage, setErrorMessage] = useState("");
+	const [editable, setEditable] = useState<boolean | null>();
 	const [basicAuth, setBasicAuth] = useState(
 		storage.get(LOCAL_STORAGE_BASIC_AUTH),
 	);
@@ -27,6 +38,53 @@ function App() {
 		status: ACTION_STATUS_SUCCESS,
 		sections: [],
 	});
+
+	const onClickChangePosition = async ({
+		sectionId,
+		direction,
+		dispatch,
+	}: {
+		direction: string;
+		dispatch: any;
+		sectionId: string;
+	}) => {
+		const authorization = `Basic ${basicAuth}`;
+
+		try {
+			const response = await graphQLCall({
+				headers: {
+					authorization,
+				},
+				query: GRAPHQL_QUERIES.CHANGE_SECTION_POSITION,
+				data: {
+					userId: FAKE_USER_EMAIL,
+					sectionId,
+					direction,
+				},
+			});
+
+			if (response.status !== 200) {
+				dispatch({
+					type: ACTION_SET_STATUS,
+					payload: {
+						status: ACTION_STATUS_ERROR,
+					},
+				});
+			} else {
+				const data = await response.json();
+				dispatch({
+					type: ACTION_GET_DATA,
+					payload: {
+						status: ACTION_STATUS_SUCCESS,
+						sections: data.data.changeSectionPosition,
+					},
+				});
+			}
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+		}
+	};
 
 	useEffect(() => {
 		document.getElementById("logo")?.classList.add("fadeOut");
@@ -186,34 +244,85 @@ function App() {
 	/**
 	 * User is fully authenticated. We can show the app.
 	 */
+
 	return (
 		<AppContext.Provider value={{ state, dispatch }}>
 			<div className="prose prose-lighter">
 				<Header>
 					<h1 className="heading mb-0 text-center">
 						My Shortcuts
-						<ButtonIcon
-							focusMode="light"
-							mode="light"
-							noBorder
-							size="small"
-							className="ml-2"
-							label="Edit section"
-							// onClick={() => {
-							// 	setEditable(
-							// 		editable === item.position ? null : item.position,
-							// 	);
-							// 	setUserInputSectionTitle(JSON.stringify(item.title, null, 2));
-							// 	setUserInputShortcuts(JSON.stringify(item.data, null, 2));
-							// }}
-						>
-							<IconEdit
-							// className="h-3 w-3"
-							/>
-						</ButtonIcon>
+						{state && state?.sections?.length > 0 && (
+							<ButtonIcon
+								focusMode="light"
+								mode="light"
+								noBorder
+								size="small"
+								className="ml-2"
+								label="Edit section"
+								onClick={() => {
+									setEditable(!editable);
+								}}
+							>
+								<IconEdit />
+							</ButtonIcon>
+						)}
 					</h1>
 				</Header>
+
 				<Main className="pt-0">
+					{state && state?.sections?.length > 0 && editable && (
+						<div className="flex flex-wrap">
+							<Table caption="Edit Sections">
+								<TableHead>
+									<TableRow>
+										<TableCell>Section</TableCell>
+										<TableCell className="text-right">Position</TableCell>
+									</TableRow>
+								</TableHead>
+
+								<TableBody>
+									{state.sections.map((section) => (
+										<TableRow key={section.position}>
+											<TableCell>{section.title}</TableCell>
+											<TableCell>
+												<div className="flex justify-end gap-2">
+													{section.position !== 1 && (
+														<ButtonIcon
+															noBorder
+															label="Move up"
+															mode="light"
+															focusMode="alt-system"
+															onClick={() => {}}
+														>
+															<IconUp monotone className="h-3 w-3" />
+														</ButtonIcon>
+													)}
+
+													{section.position !== state.sections.length && (
+														<ButtonIcon
+															noBorder
+															label="Move down"
+															mode="light"
+															focusMode="alt-system"
+															onClick={() => {
+																onClickChangePosition({
+																	sectionId: section.id,
+																	direction: "down",
+																	dispatch,
+																});
+															}}
+														>
+															<IconDown className="h-3 w-3" monotone />
+														</ButtonIcon>
+													)}
+												</div>
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</div>
+					)}
 					{state && state?.sections?.length > 0 && <Shortcuts />}
 				</Main>
 				<Footer
