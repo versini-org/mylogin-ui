e95e1a0fe99f18dadfdd6104bba7372b0537a6eb
Arno V <versini@gmail.com>
2024-04-06 14:43:58 -0400

    feat: using Card to separate sections

--- packages/client/src/modules/Shortcuts/Shortcuts.tsx
+++ packages/client/src/modules/Shortcuts/Shortcuts.tsx
@@ -1,9 +1,11 @@
 import {
 	ButtonIcon,
 	ButtonLink,
+	Card,
 	Table,
 	TableBody,
 	TableCell,
+	TableFooter,
 	TableHead,
 	TableRow,
 } from "@versini/ui-components";
@@ -15,6 +17,7 @@ import {
 	IconEdit,
 	IconUp,
 } from "@versini/ui-icons";
+import { Flexgrid, FlexgridItem } from "@versini/ui-system";
 import { useContext, useRef, useState } from "react";
 
 import { LOCAL_STORAGE_BASIC_AUTH } from "../../common/constants";
@@ -77,178 +80,204 @@ export const Shortcuts = () => {
 
 			{state.sections.map((section) => {
 				return (
-					<div key={section.id} className="mb-5">
-						<h2 className="heading text-center font-bold text-slate-200">
-							{section.title}
-							<ButtonIcon
-								focusMode="light"
-								mode="light"
-								noBorder
-								size="small"
-								className="ml-2"
-								label="Edit section"
-								onClick={() => {
-									setEditable(editable === section.id ? null : section.id);
-								}}
-							>
-								<IconEdit className="h-3 w-3" />
-							</ButtonIcon>
-						</h2>
+					<Card
+						compact
+						mode="dark"
+						key={section.id}
+						spacing={{ b: 2 }}
+						header={
+							<Flexgrid alignHorizontal="space-between">
+								<FlexgridItem>
+									<h2 className="heading">{section.title}</h2>
+								</FlexgridItem>
+								<FlexgridItem>
+									<ButtonIcon
+										noBackground
+										focusMode="light"
+										mode="light"
+										noBorder
+										size="small"
+										label={`Edit shortcuts for section ${section.title}`}
+										onClick={() => {
+											setEditable(editable === section.id ? null : section.id);
+										}}
+									>
+										<IconEdit className="h-3 w-3" />
+									</ButtonIcon>
+								</FlexgridItem>
+							</Flexgrid>
+						}
+					>
+						<div>
+							{editable && editable === section.id && (
+								<Table
+									compact
+									caption="Edit Shortcuts"
+									spacing={{ b: 5, t: 5 }}
+								>
+									<TableHead>
+										<TableRow>
+											<TableCell>Label</TableCell>
+											<TableCell>URL</TableCell>
+											<TableCell className="text-right">Move</TableCell>
+											<TableCell className="text-right">Add</TableCell>
+											<TableCell className="text-right">Delete</TableCell>
+										</TableRow>
+									</TableHead>
 
-						{editable && editable === section.id && (
-							<Table compact caption="Edit Shortcuts" spacing={{ b: 5, t: 5 }}>
-								<TableHead>
-									<TableRow>
-										<TableCell>Label</TableCell>
-										<TableCell>URL</TableCell>
-										<TableCell className="text-right">Move</TableCell>
-										<TableCell className="text-right">Add</TableCell>
-										<TableCell className="text-right">Delete</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{section.shortcuts.map((shortcut, idx) => (
-										<TableRow key={`edit-shortcut-${shortcut.id}`}>
-											<TableCell>
-												<TextInput
-													focusMode="light"
-													label="Shortcut label"
-													labelHidden
-													name={shortcut.label}
-													defaultValue={shortcut.label}
-													onChange={(e) => {
-														onChangeShortcut({
-															position: idx,
-															basicAuth,
-															label: e.target.value,
-															section,
-															dispatch,
-														});
-													}}
-												/>
-											</TableCell>
-											<TableCell>
-												<TextArea
-													textAreaClassName="font-mono text-sm"
-													focusMode="light"
-													label="URL"
-													name={shortcut.url}
-													defaultValue={shortcut.url}
-													onChange={(e) => {
-														onChangeShortcut({
-															position: idx,
-															basicAuth,
-															url: e.target.value,
-															section,
-															dispatch,
-														});
-													}}
-												/>
-											</TableCell>
-											<TableCell className="text-right">
-												<div className="flex justify-end gap-2">
-													{idx !== 0 && (
-														<ButtonIcon
-															noBorder
-															label="Move up"
-															mode="light"
-															focusMode="alt-system"
-															onClick={() => {
-																onClickChangeShortcutPosition({
-																	basicAuth,
-																	section,
-																	direction: "up",
-																	dispatch,
-																	position: idx,
-																});
-															}}
-														>
-															<IconUp monotone className="h-3 w-3" />
-														</ButtonIcon>
-													)}
-
-													{idx !== section.shortcuts.length - 1 && (
-														<ButtonIcon
-															noBorder
-															label="Move down"
-															mode="light"
-															focusMode="alt-system"
-															onClick={() => {
-																onClickChangeShortcutPosition({
-																	basicAuth,
-																	section,
-																	direction: "down",
-																	dispatch,
-																	position: idx,
-																});
-															}}
-														>
-															<IconDown className="h-3 w-3" monotone />
-														</ButtonIcon>
-													)}
-												</div>
-											</TableCell>
-											<TableCell className="text-right">
-												<div className="flex justify-end">
-													<ButtonIcon
-														mode="light"
-														focusMode="alt-system"
-														noBorder
-														label="New Shortcut"
-														onClick={() => {
-															onClickAddShortcut({
-																basicAuth,
-																dispatch,
+									<TableBody>
+										{section.shortcuts.map((shortcut, idx) => (
+											<TableRow key={`edit-shortcut-${shortcut.id}`}>
+												<TableCell>
+													<TextInput
+														focusMode="light"
+														label="Shortcut label"
+														labelHidden
+														name={shortcut.label}
+														defaultValue={shortcut.label}
+														onChange={(e) => {
+															onChangeShortcut({
 																position: idx,
+																basicAuth,
+																label: e.target.value,
 																section,
+																dispatch,
 															});
 														}}
-													>
-														<IconAdd className="h-3 w-3" monotone />
-													</ButtonIcon>
-												</div>
-											</TableCell>
-											<TableCell className="text-right">
-												<ButtonIcon
-													focusMode="light"
-													mode="light"
-													noBorder
-													label="Delete shortcut"
-													disabled={section.shortcuts.length === 1}
-													onClick={() => {
-														shortcutPositionRef.current = idx;
-														sectionWithShortcutRef.current = section;
-														setShowConfirmation(true);
-													}}
-												>
-													<div className="text-red-400">
-														<IconDelete className="h-3 w-3" monotone />
+													/>
+												</TableCell>
+												<TableCell>
+													<TextArea
+														textAreaClassName="font-mono text-sm"
+														focusMode="light"
+														label="URL"
+														name={shortcut.url}
+														defaultValue={shortcut.url}
+														onChange={(e) => {
+															onChangeShortcut({
+																position: idx,
+																basicAuth,
+																url: e.target.value,
+																section,
+																dispatch,
+															});
+														}}
+													/>
+												</TableCell>
+												<TableCell className="text-right">
+													<div className="flex justify-end gap-2">
+														{idx !== 0 && (
+															<ButtonIcon
+																noBorder
+																label="Move up"
+																mode="light"
+																focusMode="alt-system"
+																onClick={() => {
+																	onClickChangeShortcutPosition({
+																		basicAuth,
+																		section,
+																		direction: "up",
+																		dispatch,
+																		position: idx,
+																	});
+																}}
+															>
+																<IconUp monotone className="h-3 w-3" />
+															</ButtonIcon>
+														)}
+
+														{idx !== section.shortcuts.length - 1 && (
+															<ButtonIcon
+																noBorder
+																label="Move down"
+																mode="light"
+																focusMode="alt-system"
+																onClick={() => {
+																	onClickChangeShortcutPosition({
+																		basicAuth,
+																		section,
+																		direction: "down",
+																		dispatch,
+																		position: idx,
+																	});
+																}}
+															>
+																<IconDown className="h-3 w-3" monotone />
+															</ButtonIcon>
+														)}
 													</div>
-												</ButtonIcon>
+												</TableCell>
+												<TableCell className="text-right">
+													<div className="flex justify-end">
+														<ButtonIcon
+															mode="light"
+															focusMode="alt-system"
+															noBorder
+															label="New Shortcut"
+															onClick={() => {
+																onClickAddShortcut({
+																	basicAuth,
+																	dispatch,
+																	position: idx,
+																	section,
+																});
+															}}
+														>
+															<IconAdd className="h-3 w-3" monotone />
+														</ButtonIcon>
+													</div>
+												</TableCell>
+												<TableCell className="text-right">
+													<ButtonIcon
+														focusMode="light"
+														mode="light"
+														noBorder
+														label="Delete shortcut"
+														disabled={section.shortcuts.length === 1}
+														onClick={() => {
+															shortcutPositionRef.current = idx;
+															sectionWithShortcutRef.current = section;
+															setShowConfirmation(true);
+														}}
+													>
+														<div className="text-red-400">
+															<IconDelete className="h-3 w-3" monotone />
+														</div>
+													</ButtonIcon>
+												</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+
+									<TableFooter>
+										<TableRow>
+											<TableCell colSpan={5} className="text-center uppercase">
+												{section.shortcuts.length} shortcuts
 											</TableCell>
 										</TableRow>
-									))}
-								</TableBody>
-							</Table>
-						)}
-						<div className="flex flex-wrap justify-center">
-							{section.shortcuts.map((shortcut, idx) => {
-								return (
-									<ButtonLink
-										focusMode="light"
-										key={`${shortcut.url}-${shortcut.label}-${idx}`}
-										noBorder
-										link={shortcut.url}
-										target="_blank"
-										className="mr-1 mt-1 w-44 sm:w-52"
-										maxLabelLength={23}
-									>
-										{shortcut.label}
-									</ButtonLink>
-								);
-							})}
+									</TableFooter>
+								</Table>
+							)}
+							<div className="flex flex-wrap justify-center">
+								{section.shortcuts.map((shortcut, idx) => {
+									return (
+										<ButtonLink
+											key={`${shortcut.url}-${shortcut.label}-${idx}`}
+											mode="dark"
+											focusMode="light"
+											noBorder
+											link={shortcut.url}
+											target="_blank"
+											className="mr-1 mt-1 w-44 sm:w-52"
+											maxLabelLength={23}
+										>
+											{shortcut.label}
+										</ButtonLink>
+									);
+								})}
+							</div>
 						</div>
-					</div>
+					</Card>
 				);
 			})}
 		</>
