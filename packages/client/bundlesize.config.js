97ec480a13972037859166a553296441ab306198
Arno V <versini@gmail.com>
2024-07-07 13:57:02 -0400

    Update bundlesize.config.js

--- packages/client/bundlesize.config.js
+++ packages/client/bundlesize.config.js
@@ -9,15 +9,19 @@ export default {
 		 */
 		{
 			path: "dist/static/js/index.<hash>.js",
-			limit: "5 kb",
+			limit: "6 kb",
 		},
 		{
 			path: "dist/static/js/lib-react.<hash>.js",
 			limit: "45 kb",
 		},
 		{
-			path: "dist/static/js/vendors-*auth-provider*.<hash>.js",
-			limit: "55 kb",
+			path: "dist/static/js/lib-router.<hash>.js",
+			limit: "20 kb",
+		},
+		{
+			path: "dist/static/js/vendors-*uuid*.<hash>.js",
+			limit: "43 kb",
 		},
 		/**
 		 * JavaScript static async assets.
@@ -27,12 +31,8 @@ export default {
 			limit: "5 kb",
 		},
 		{
-			path: "dist/static/js/async/vendors-*uuid*.<hash>.js",
-			limit: "32 kb",
-		},
-		{
-			path: "dist/static/js/async/node_modules_*ui-components*.<hash>.js",
-			limit: "3 kb",
+			path: "dist/static/js/async/vendors-*node_modules_*ui-components*.<hash>.js",
+			limit: "6 kb",
 		},
 		/**
 		 * CSS static assets.
