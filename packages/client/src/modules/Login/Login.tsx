d40f7ba8fa0647c7849cfb370f7dcd7a5f6c0b06
Arno V <versini@gmail.com>
2024-07-15 13:32:59 -0400

    feat: adding support for Passkey

--- packages/client/src/modules/Login/Login.tsx
+++ packages/client/src/modules/Login/Login.tsx
@@ -1,6 +1,6 @@
-import { Button, ButtonIcon, Main } from "@versini/ui-components";
+import { Button, ButtonIcon, Card, Main } from "@versini/ui-components";
 import { TextInput, TextInputMask } from "@versini/ui-form";
-import { IconHide, IconShow } from "@versini/ui-icons";
+import { IconHide, IconKey, IconShow } from "@versini/ui-icons";
 import { Flexgrid, FlexgridItem } from "@versini/ui-system";
 import { useContext, useEffect, useState } from "react";
 
@@ -9,13 +9,17 @@ import {
 	ACTION_SET_STATUS,
 	ACTION_STATUS_SUCCESS,
 } from "../../common/constants";
-import { LOG_IN, PASSWORD_PLACEHOLDER } from "../../common/strings";
+import {
+	LOG_IN,
+	LOG_IN_PASSKEY,
+	PASSWORD_PLACEHOLDER,
+} from "../../common/strings";
 import { AppContext } from "../App/AppContext";
 import { AppFooter } from "../Common/AppFooter";
 import { AppHeader } from "../Common/AppHeader";
 
 export const Login = () => {
-	const { login, logoutReason } = useAuth();
+	const { login, logoutReason, loginWithPasskey } = useAuth();
 
 	const { dispatch } = useContext(AppContext);
 
@@ -66,74 +70,96 @@ export const Login = () => {
 		<>
 			<AppHeader />
 			<Main>
-				<form className="mx-auto" onSubmit={handleLogin}>
-					<Flexgrid rowGap={7} width="350px">
-						<FlexgridItem span={12}>
-							{globalErrorMessage && (
-								<div className="p-2 text-sm text-center text-copy-error-light bg-surface-darker">
-									{globalErrorMessage}
-								</div>
-							)}
-						</FlexgridItem>
+				<form className="mt-5" onSubmit={handleLogin}>
+					<Flexgrid rowGap={7} alignHorizontal="center">
+						<FlexgridItem span={6}>
+							<Card mode="dark">
+								<FlexgridItem span={12}>
+									{globalErrorMessage && (
+										<div className="p-2 text-sm text-center text-copy-error-light bg-surface-darker">
+											{globalErrorMessage}
+										</div>
+									)}
+								</FlexgridItem>
 
-						<FlexgridItem span={12}>
-							<TextInput
-								required
-								autoCapitalize="off"
-								autoComplete="off"
-								autoCorrect="off"
-								mode="dark"
-								focusMode="light"
-								name="username"
-								label="Username"
-								onChange={(e) => {
-									setSimpleLogin({
-										...simpleLogin,
-										username: e.target.value,
-									});
-									setErrorMessage("");
-								}}
-								error={errorMessage !== ""}
-							/>
-						</FlexgridItem>
-						<FlexgridItem span={12}>
-							<TextInputMask
-								required
-								autoCapitalize="off"
-								autoComplete="off"
-								autoCorrect="off"
-								mode="dark"
-								focusMode="light"
-								name="password"
-								label={PASSWORD_PLACEHOLDER}
-								rightElement={
-									<ButtonIcon focusMode="light">
-										{masked ? <IconShow /> : <IconHide />}
-									</ButtonIcon>
-								}
-								onMaskChange={setMasked}
-								onChange={(e) => {
-									setSimpleLogin({
-										...simpleLogin,
-										password: e.target.value,
-									});
-									setErrorMessage("");
-								}}
-								error={errorMessage !== ""}
-								helperText={errorMessage}
-							/>
-						</FlexgridItem>
+								<FlexgridItem span={12}>
+									<TextInput
+										required
+										autoCapitalize="off"
+										autoComplete="off"
+										autoCorrect="off"
+										mode="dark"
+										focusMode="light"
+										name="username"
+										label="Username"
+										onChange={(e) => {
+											setSimpleLogin({
+												...simpleLogin,
+												username: e.target.value,
+											});
+											setErrorMessage("");
+										}}
+										error={errorMessage !== ""}
+									/>
+								</FlexgridItem>
+								<FlexgridItem span={12}>
+									<TextInputMask
+										required
+										autoCapitalize="off"
+										autoComplete="off"
+										autoCorrect="off"
+										mode="dark"
+										focusMode="light"
+										name="password"
+										label={PASSWORD_PLACEHOLDER}
+										rightElement={
+											<ButtonIcon focusMode="light">
+												{masked ? <IconShow /> : <IconHide />}
+											</ButtonIcon>
+										}
+										onMaskChange={setMasked}
+										onChange={(e) => {
+											setSimpleLogin({
+												...simpleLogin,
+												password: e.target.value,
+											});
+											setErrorMessage("");
+										}}
+										error={errorMessage !== ""}
+										helperText={errorMessage}
+									/>
+								</FlexgridItem>
 
-						<FlexgridItem span={12}>
-							<Button
+								<FlexgridItem span={12}>
+									<Button
+										mode="light"
+										focusMode="light"
+										fullWidth
+										noBorder
+										type="submit"
+										className="mb-4 mt-6"
+									>
+										{LOG_IN}
+									</Button>
+								</FlexgridItem>
+							</Card>
+						</FlexgridItem>
+					</Flexgrid>
+
+					<div className="text-center text-copy-light">or</div>
+					<Flexgrid alignHorizontal="center">
+						<FlexgridItem span={6}>
+							<ButtonIcon
+								mode="dark"
 								focusMode="light"
 								fullWidth
 								noBorder
-								type="submit"
-								className="mb-4 mt-6"
+								className="mb-4 mt-1"
+								labelRight={LOG_IN_PASSKEY}
+								onClick={loginWithPasskey}
 							>
-								{LOG_IN}
-							</Button>
+								<IconKey className="size-4" />
+							</ButtonIcon>
 						</FlexgridItem>
 					</Flexgrid>
 				</form>
